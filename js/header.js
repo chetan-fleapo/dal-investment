fde81669e199e2a701a7d8528e467ed0932e9337
chetan mane <kpcl2263@gmail.com>
2021-05-31 21:20:26 +0530

    changes done

--- js/header.js
+++ js/header.js
@@ -5,31 +5,37 @@ class Header extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-      <div class="page-wrapper">
-            <div class="container">
-                <div class="nav-wrapper">
+        <div class="page-wrapper">
+          <div class="container">
+              <div class="nav-wrapper">
                 <div class="grad-bar"></div>
-                <nav class="navbar">
-                  <a href="index.html" class="logo">
-                  <img src="img/logo.png" alt="Company Logo">
-                  </a>
-                <div class="menu-toggle" id="mobile-menu">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
+                    <nav class="navbar">
+                      <a href="index.html" class="logo">
+                      <img src="img/logo.png" alt="Company Logo">
+                      </a>
+                      <div class="menu-toggle" id="mobile-menu">
+                          <span class="bar"></span>
+                          <span class="bar"></span>
+                          <span class="bar"></span>
+                      </div>
+                      <ul class="nav no-search">
+                          <li class="nav-item"><a href="index.html">Home</a></li>
+                          <li class="nav-item"><a href="about-us.html">About Us</a></li>
+                          <li class="nav-item"><a href="investment-philosophy.html">investment philosophy</a></li>
+                          <li class="nav-item"><a href="careers.html">Careers</a></li>
+                          <li class="nav-item"><a href="team.html">team</a></li>
+                          <li class="nav-item"><a href="contact-us.html">Contact Us</a></li>
+                          <div class="floow-us-on">
+                          <div class="inner-wrap d-flex justify-content-between align-items-center">  
+                              <span>Follow Us On</span>
+                              <a href=""> <i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                          </div>
+                      </ul>
+                    </nav>
                 </div>
-                <ul class="nav no-search">
-                    <li class="nav-item"><a href="index.html">Home</a></li>
-                    <li class="nav-item"><a href="about-us.html">About Us</a></li>
-                    <li class="nav-item"><a href="investment-philosophy.html">investment philosophy</a></li>
-                    <li class="nav-item"><a href="careers.html">Careers</a></li>
-                    <li class="nav-item"><a href="team.html">team</a></li>
-                    <li class="nav-item"><a href="contact-us.html">Contact Us</a></li>
-                </ul>
-                </nav>
-                </div>
-                </div>
-           </div>
+              </div>
+          </div>
       `;
     }
   }
