c9d8f5090c116691e25d5d2271d6aba5cca5acbc
Belal Ahmad <belalahmad.fleapo@gmail.com>
2021-06-02 19:54:11 +0530

    fixed issues

--- js/active.js
+++ js/active.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    var path = window.location.href; 
-    // because the 'href' property of the DOM element is the absolute path
-    $('ul a').each(function() {
-      if (this.href === path) {
-        $(this).addClass('active');
-      }
-    });
-})
+$(document).ready(function () {
+  var path = window.location.href;
+  // because the 'href' property of the DOM element is the absolute path
+  $("ul.nav a").each(function () {
+    if (this.href === path) {
+      $(this).addClass("active");
+    }
+  });
+});
